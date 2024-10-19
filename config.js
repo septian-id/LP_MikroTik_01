fab3199a968227a9f8d041b2e3010c8089ddd240
Septiana Harun <55664528+septian-id@users.noreply.github.com>
2024-10-20 02:22:15 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,49 @@
+var config = {
+clear_session       : true,
+api_url             : 'https://desktop.niceradius.com/api',
+api_key             : 'kXhka4N8e2V+i2VraWWHaA==',
+router_id           : 'W9MZ2M7IEEZROLS8',
+merchant_code       : 'D14938',
+merchant_api_key    : '54e8e5bd05a028deda86cf567df95302',
+fonnte_token        : 'zf!v_rWqWCDeZhF1GYou',
+contact             : 'https://wa.me/6283123456789',
+currency            : 'Rp',
+color               : 12
+}
+
+/*
+AUTENTIKASI======================
+    api_key (Dari https://desktop.niceradius.com)
+    router_id (Dari https://desktop.niceradius.com)
+    
+    
+PAYMENT GATEWAY==================
+    merchant_code (Dari https://dashboard.duitku.com)
+    merchant_api_key (Dari https://dashboard.duitku.com)
+
+
+NOTIFIKASI TRANSAKSI=============
+    fonnte_token (Dari https://fonnte.com)
+    
+    
+PILIHAN WARNA ==================
+    0 = amber
+    1 = black
+    2 = red",
+    3 = blue-grey
+    4 = blue
+    5 = cyan
+    6 = dark-grey
+    7 = deep-orange
+    8 = deep-purple
+    9 = green
+    10 = indigo
+    11 = light-blue
+    12 = light-green
+    13 = lime
+    14 = orange
+    15 = pink
+    16 = purple
+    17 = teal
+    18 = yellow
+*/
